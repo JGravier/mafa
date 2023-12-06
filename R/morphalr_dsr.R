5707159f7f22523a61e64b3ef271ab17cc3cbb7d
JGravier <JGravier@users.noreply.github.com>
2023-12-06 15:50:07 +0100

    create DSR

---
+++ R/morphalr_dsr.R
@@ -0,0 +1,41 @@
+#' Compute distance to smallest bounding rectangle
+#'
+#' @description
+#' Compute distance to smallest bounding rectangle (DSR).
+#'
+#' @param sfpolygons A sf object composed of polygons
+#' @return `sfpoygons` with DSR as a new variable
+#'
+#' @references Ali, Atef Bel Hadj. 2001. « Qualité géométrique des entités géographiques surfaciques. Application à l’appariement et définition d’une typologie des écarts geométriques ». Phdthesis, université Gustave Effeil ; Anciennement Université de Marne La vallée. [https://hal.science/tel-03244834](https://hal.science/tel-03244834), p. 92.
+#' @references Robert, Sandrine, Éric Grosso, Pascal Chareille, et Hélène Noizet. 2014. « MorphAL (Morphological Analysis) : un outil d’analyse de morphologie urbaine ». In Archéologie de l’espace urbain, édité par Elisabeth Lorans et Xavier Rodier, 451‑63. Perspectives Villes et Territoires. Tours: Presses universitaires François-Rabelais. [https://doi.org/10.4000/books.pufr.7717]( https://doi.org/10.4000/books.pufr.7717).
+#'
+#' @importFrom Rdpack reprompt
+#' @importFrom rlang .data
+#' @export
+
+morphalr_dsr <- function(sfpolygons){
+  # create a list of polygons
+  listpolygons <- sfpolygons |>
+    tibble::rowid_to_column(var = 'rowidboundingbox') |>
+    dplyr::group_by(.data$rowidboundingbox) |>
+    dplyr::group_split()
+
+  listboundingbox <- lapply(listpolygons, morphalr_utils_mbboxoriented) # apply morphal_utils_mmboxoriented function
+
+  sfboudingbox <- dplyr::bind_rows(listboundingbox)
+
+  #### compute DSR
+  area_bbox <- sfboudingbox |>
+    dplyr::mutate(area_bbox = as.numeric(sf::st_area(x=.data$geometry)))
+
+  sfpolygons <- sfpolygons |>
+    dplyr::mutate(area_polygon = as.numeric(sf::st_area(x=sf::st_geometry(sfpolygons)))) |>
+    dplyr::bind_cols(
+      area_bbox |> sf::st_drop_geometry()
+    ) |>
+    dplyr::mutate(dsr = 1 - (.data$area_polygon/.data$area_bbox) ) |>
+    dplyr::select(-.data$area_bbox, -.data$area_polygon)
+
+  return(sfpolygons)
+
+}
---
+++ R/morphalr_utils.R
@@ -0,0 +1,32 @@
+#' Create minBBox of a polygon
+#'
+#' @description
+#' Create minimal bounding oriented box rectangles of a polygon
+#'
+#' @param sfpolygon A sf object composed of polygon
+#' @return A sf object composed of minimal bounding box oriented rectangle
+#'
+#' @importFrom Rdpack reprompt
+#' @importFrom rlang .data
+#' @export
+
+morphalr_utils_mbboxoriented <- function(sfpolygon){
+  #### extract minimal bounding box
+  xyminbbox <- shotGroups::getMinBBox(as.matrix(sf::st_coordinates(sfpolygon)[,1:2]))$pts
+  # add a point to close polygon
+  xyminbbox <- rbind(xyminbbox, xyminbbox[1,])
+  # create polygon as sf object
+  minbboxpolygon <- sf::st_sfc(
+      sf::st_polygon(
+        list(
+          cbind(
+            xyminbbox[,1],
+            xyminbbox[,2]
+            ))),
+    crs = sf::st_crs(sfpolygon)
+    )
+  minbboxpolygon <- sf::st_as_sf(minbboxpolygon) |>
+    dplyr::rename(geometry = .data$x)
+
+  return(minbboxpolygon)
+}
